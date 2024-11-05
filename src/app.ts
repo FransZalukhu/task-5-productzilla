e9fce0596eb67ce3b46ce2f60160104808c88ce1
FransZalukhu <fransdamai2020@gmail.com>
2024-11-05 12:13:51 +0700

    update main.yml and add docker

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ connectDB();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
